efef141a2c8349568cade8e56240721ad65426f8
danil <d.prytckov@yandex.ru>
2020-10-22 17:00:43 +0500

    task (3)

--- build/js/main.js
+++ build/js/main.js
@@ -19043,6 +19043,18 @@ let phone1 = $(".calc .phone");
 let phone2 = $(".support .phone");
 let phone3 = $(".modal-content .phone");
 
+if (phone1.val() === '') {
+    phone1.val('+7 (');
+}
+
+if (phone2.val() === '') {
+    phone2.val('+7 (');
+}
+
+if (phone3.val() === '') {
+    phone3.val('+7 (');
+}
+
 phones.on("focus", function () {
     phones.attr("placeholder", "");
 
@@ -19063,16 +19075,16 @@ phones.on("blur", function () {
     phone1.attr("placeholder", "НОМЕР ТЕЛЕФОНА")
     phone2.attr("placeholder", "номер телефона")
 
-    if (phone1.val() === '+7 (') {
-        phone1.val('')
+    if (phone1.val() === '+7 ' || phone1.val() === '+7' || phone1.val() === '+' || phone1.val() === '') {
+        phone1.val('+7 (')
     }
 
-    if (phone2.val() === '+7 (') {
-        phone2.val('')
+    if (phone2.val() === '+7 ' || phone2.val() === '+7' || phone2.val() === '+' || phone2.val() === '') {
+        phone2.val('+7 (')
     }
 
-    if (phone3.val() === '+7 (') {
-        phone3.val('')
+    if (phone3.val() === '+7 ' || phone3.val() === '+7' || phone3.val() === '+' || phone3.val() === '') {
+        phone3.val('+7 (')
     }
 });
 
